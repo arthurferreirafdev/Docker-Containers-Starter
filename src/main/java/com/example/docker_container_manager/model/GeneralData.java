e3ad0e81e124e739eb1f9dd48fb38c09ca5ebe54
arthur <arthurfsilva110@gamial.com>
2025-08-29 16:03:55 -0300

    removing unuseless files

--- src/main/java/com/example/docker_container_manager/model/GeneralData.java
+++
@@ -1,8 +0,0 @@
-package com.example.docker_container_manager.model;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
-public class GeneralData {
-    public static String dockerLogPrefixPath;
-}
