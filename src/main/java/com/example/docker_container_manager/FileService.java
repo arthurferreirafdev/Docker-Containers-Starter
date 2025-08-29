c885a5ee5023bfea6360a360bf8b132f5f6c9f5f
arthur <arthurfsilva110@gamial.com>
2025-08-29 15:58:30 -0300

    fix: file path

--- src/main/java/com/example/docker_container_manager/FileService.java
+++ src/main/java/com/example/docker_container_manager/FileService.java
@@ -1,21 +1,27 @@
 package com.example.docker_container_manager;
 
-import com.example.docker_container_manager.model.GeneralData;
-import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.io.InputStreamReader;
 
+@Component
 public class FileService {
+    static String dockerLogPrefixPath;
+
     public static void writeFile(String content, String fileName){
-        String filePath = GeneralData.dockerLogPrefixPath + fileName;
+        String filePath = dockerLogPrefixPath + fileName;
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true)){}) {
             writer.write(content);
             System.out.println("Successfully wrote content to " + filePath);
         }catch (Exception e){
             System.out.println("Error in file Write: "  + e.getMessage());
         }
+    }
+
+    @Value("${docker.file.output}")
+    public void setDockerLogPrefixPath(String path) {
+        FileService.dockerLogPrefixPath = path;
     }
 }
